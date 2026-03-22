20ee42169ee022d3188e9249f1b51b45d857ef0a
AlexM4 <zhouchuanglin@gmail.com>
2026-03-22 23:08:14 +0100

    模拟电视， 10秒载入

--- iyf.tv/MainPage.xaml.cs
+++ iyf.tv/MainPage.xaml.cs
@@ -1,10 +1,29 @@
 ﻿namespace iyf.tv;
 
 public partial class MainPage : ContentPage
-{
-     public MainPage()
+{ 
+    public MainPage()
     {
         InitializeComponent();
+#if ANDROID
+        webView.HandlerChanged += (s, e) =>
+        {
+            if (webView.Handler?.PlatformView is Android.Webkit.WebView nativeWebView)
+            {
+                nativeWebView.Settings.UserAgentString = "Mozilla/5.0 (Linux; Android TV 10) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36";
+            }
+        };
+#endif
+        //webView.Source = null;
+        _ = NavigateAfterDelayAsync();
     }
-     
+
+    private async Task NavigateAfterDelayAsync()
+    {
+        await Task.Delay(10000);
+        MainThread.BeginInvokeOnMainThread(() =>
+        {
+            webView.Source = "https://tv.yfsp.tv";
+        });
+    }
 }
